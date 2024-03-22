c25bf44687bc84d47fae3a9403890da67772c381
Hriday Sheth <hssheth29@gmail.com>
2024-03-22 18:40:40 +0530

    Modified logic using which.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use std::process::Command;
 fn main() {
     let osquery_installed = check_osquery_installed();
 
-
-    println!("osquery is {} available.", if osquery_installed { "" } else { "not " });
+    println!("osquery is {}available.", if osquery_installed { "" } else { "not " });
 }
 
 fn check_osquery_installed() -> bool {
-    let output = Command::new("osqueryi")
-        .arg("--version")
-        .output()
-        .unwrap_or_else(|_| panic!("Failed to execute 'osqueryi' version"));
+    let output = Command::new("which")
+        .arg("osqueryi")
+        .output();
 
-    output.status.success()
+    match output {
+        Ok(output) => output.status.success(),
+        Err(_) => false,
+    }
 }
-
