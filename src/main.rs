52e86ccb27f95b7070e782f39789106e798891a6
Hriday Sheth <hssheth29@gmail.com>
2024-03-23 13:52:36 +0530

    feat: Regex input output logic.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 use std::process::Command;
+use regex::Regex;
 
 fn main() {
     let osquery_installed = check_osquery_installed();
-
     println!("osquery is {}available.", if osquery_installed { "" } else { "not " });
 }
 
 fn check_osquery_installed() -> bool {
     let output = Command::new("which")
         .arg("osqueryi")
-        .output();
+        .output()
+        .expect("Failed to execute 'which'");
 
-    match output {
-        Ok(output) => output.status.success(),
-        Err(_) => false,
+    if !output.status.success() {
+        return false;
     }
+
+    let output_str = String::from_utf8_lossy(&output.stdout);
+    let re = Regex::new(r"/\w+").unwrap(); // Simple regex to check for a path
+    re.is_match(&output_str)
 }
+
+
+
